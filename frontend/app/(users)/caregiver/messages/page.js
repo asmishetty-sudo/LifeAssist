5fbf76e80949dcec42168d97a1e9692b8e5bb88b
Asmi Shetty <asmishetty49@gmail.com>
2026-03-20 22:03:49 +0530

    Update page.js

--- frontend/app/(users)/caregiver/messages/page.js
+++ frontend/app/(users)/caregiver/messages/page.js
@@ -60,7 +60,10 @@ export default function MessagesPage() {
         msg.chat === activeChatRef.current._id &&
         msg.sender !== profile.id
       ) {
-        socket.current.emit("seen_messages", activeChatRef.current._id);
+        socket.current.emit("seen_messages", {
+  chatId: activeChatRef.current._id,
+  userId: user.userId,
+});
         markMessagesRead();
       } else if (msg.sender !== profile.id) {
         // Increment unread count for chats not open
