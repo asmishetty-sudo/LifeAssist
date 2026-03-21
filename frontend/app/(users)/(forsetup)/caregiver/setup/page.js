5769e3a4c2649e8b482231d86664592adc78cb16
Asmi Shetty <asmishetty49@gmail.com>
2026-03-21 16:26:35 +0530

    Update page.js

--- frontend/app/(users)/(forsetup)/caregiver/setup/page.js
+++ frontend/app/(users)/(forsetup)/caregiver/setup/page.js
@@ -388,7 +388,7 @@ export default function CaregiverSetup() {
           <button
             type="submit"
             disabled={loading}
-            className="w-fit sm:w-1/4 bg-blue-500 text-white py-4 sm:py-2  text-bold rounded hover:opacity-90"
+            className="w-fit sm:w-1/4 bg-blue-500 text-white px-4 py-2  text-bold rounded hover:opacity-90"
           >
             {loading ? "Submitting..." : "Submit Profile"}
           </button>
