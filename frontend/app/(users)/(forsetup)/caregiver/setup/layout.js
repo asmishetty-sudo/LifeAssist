a0f15ab4f8dda4683e82dc8d2b86e93554b4b1e3
Asmi Shetty <asmishetty49@gmail.com>
2026-03-21 16:09:02 +0530

    Update layout.js

--- frontend/app/(users)/(forsetup)/caregiver/setup/layout.js
+++ frontend/app/(users)/(forsetup)/caregiver/setup/layout.js
@@ -5,7 +5,7 @@ export default function CaregiverLayout({ children }) {
   return (
     <div className="flex ">
       <CaregiverProvider>      
-      <main className="flex-1 p-6 min-h-[calc(100vh-7rem)] ">
+      <main className="flex-1 sm:p-6 min-h-[calc(100vh-7rem)] ">
         {children}
       </main>
       </CaregiverProvider>
